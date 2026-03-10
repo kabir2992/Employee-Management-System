f9b18b47985ff791e1c9461b80168a9033b20d5f
Kabir Sheth <kabirsheth2992@Kabirs-MacBook-Air.local>
2026-03-10 17:36:32 +0530

    Final

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId = {import.meta.env.VITE_GOOGLE_CLIENT_ID}>
   <ThemeProvider>
   <LoadingProvider>
